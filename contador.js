383c98b721727637f9ab2ea6036ec5fc21710950
Dereemii <leshermira@gmail.com>
2020-10-31 15:07:47 -0300

    first commit

---
+++ contador.js
@@ -0,0 +1,85 @@
+ 
+
+
+//CYBER 2020 - CONTADOR
+if (
+    document.getElementById("day") &&
+    document.getElementById("hour") &&
+    document.getElementById("minute") &&
+    document.getElementById("second")
+) {
+    console.log("cronometro cyber ok");
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    let countDown = new Date("October 31, 2020 15:07:00").getTime(),
+        x = setInterval(function() {
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            (document.getElementById("day").innerText = Math.floor(distance / day)),
+            (document.getElementById("hour").innerText = Math.floor(
+                (distance % day) / hour
+            )),
+            (document.getElementById("minute").innerText = Math.floor(
+                (distance % hour) / minute
+            )),
+            (document.getElementById("second").innerText = Math.floor(
+                (distance % minute) / second
+            ));
+                //do something later when date is reached
+            if (distance < 0) {
+                clearInterval(x);
+                document.querySelector('.contadorCyber').classList.add('oculta');
+                document.querySelector('#ofertaFlash1').classList.add('contadorCyber'); 
+ 
+            /* ---------------OFERTA FLASH 1 ------------------- */
+if (
+    document.getElementById("hour1") &&
+    document.getElementById("minute1") &&
+    document.getElementById("second1")
+) {
+    console.log("encontrado crono flash 1");
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    let countDown = new Date("October 31, 2020 17:00:00").getTime(),
+        x = setInterval(function() {
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+         /*    (document.getElementById("day1").innerText = Math.floor(distance / day)), */
+            (document.getElementById("hour1").innerText = Math.floor(
+                (distance % day) / hour
+            )),
+            (document.getElementById("minute1").innerText = Math.floor(
+                (distance % hour) / minute
+            )),
+            (document.getElementById("second1").innerText = Math.floor(
+                (distance % minute) / second
+            ));
+                //do something later when date is reached
+            if (distance < 0) {
+                clearInterval(x);
+               document.querySelector('.contadorCyber').classList.add('oculta'); 
+            }
+            
+        
+        }, second);
+} else {
+    console.log("falta cronometro flash 1");
+}
+
+           
+            }
+            
+        
+        }, second);
+} else {
+    console.log("No existe cronómetro");
+}
+
